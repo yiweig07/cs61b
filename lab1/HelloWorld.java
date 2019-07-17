2a7086c704a18d522e993bfc6c9a420e8c47dee7
Yiwei Gao <ivygao07@gmail.com>
2019-07-17 17:24:59 -0500

    lab1

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HelloWorld {
+	
+	public static void main (String[] args) {
+		System.out.println("Hello world!");
+		//static: does not depend on object
+		//(String[] args):parameter--> String array 
+		
+		
+	}
+
+}
